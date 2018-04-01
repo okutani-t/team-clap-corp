e50fd6a0f91179448e02061ba085da4aca2fca4c
nobu <>
2018-04-01 18:06:07 +0900

    ball appears randomly

--- js/bg-ball.js
+++ js/bg-ball.js
@@ -1,28 +1,35 @@
 window.onload = function() {
   // 使用するcanvasを取得
   var canvas = document.getElementById("bg_ball");
-  canvas.width = $(window).width();
-  canvas.height = $(window).height();
+  var windowWidth = $(window).width();
+  var windowHeight = $(window).height();
 
-
+  canvas.width = windowWidth;
+  canvas.height = windowHeight;
   //canvas.width = $(window).width() + 'px';
   //canvas.height = $(window).height() + 'px';
+
+  //ランダムで数字を出す
+  var randRange = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
 
   // ステージを関連付ける
   var stage = new createjs.Stage( canvas );
 
   // 赤い丸を描画
   var circle = new createjs.Shape();
-  circle.graphics.beginFill("#FF0000");
+  circle.graphics.beginFill("#ffc600");
   circle.graphics.drawCircle(0, 0, 50);
   circle.graphics.endFill();
-  circle.x = 100;
-  circle.y = 100;
+  circle.x = randRange(0, windowWidth);
+  circle.y = randRange(0, windowHeight);
   stage.addChild(circle);
 
   // フェードイン
   circle.alpha = 0;
-  createjs.Tween.get(circle).to({alpha:1}, 3000);
+  createjs.Tween.get(circle).to({alpha:1}, 1000);
 
   // フレームレート30で、ステージを更新
   createjs.Ticker.setFPS(30);
