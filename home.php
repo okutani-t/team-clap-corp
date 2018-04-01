07cb1358fcb2274762d60ae66cded516c83ed116
Nobu-Ku <35137437+Nobu-Ku@users.noreply.github.com>
2018-04-01 17:00:28 +0900

    フッターを中心に。h1のimgをphp内に移動しました。 (#16)
    * footerを中心にいくように変更
    
    * cosume-h1
    
    * footerとh1を修正
    
    * cosume
    
    * cosume
    
    * cosume

--- home.php
+++ home.php
@@ -1,9 +1,10 @@
 <?php get_header(); ?>
      <header id="header">
        <div class="top-header clearfix">
-          <h1 class="logo">
-            <a href="#">TEAM CLAP</a>
-          </h1>
+         <a href="#">
+          <h1 class="logo">TEAM CLAP</h1>
+          <img class="h1-img" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.jpg" alt="teamclap">
+         </a>
           <nav class="global-nav">
             <ul>
               <li><a class="first-item" href="#about">ABOUT</a></li>
