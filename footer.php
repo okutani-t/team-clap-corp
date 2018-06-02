72ed20050f356f281633285d821697445c4c17fd
okutani-t <okutani.nt@gmail.com>
2018-06-02 18:39:21 +0900

    Tweak styles

--- footer.php
+++ footer.php
@@ -1,64 +1,64 @@
 <?php
-/**
- * Version    : 1.0.1
- * Author     : inc2734
- * Author URI : http://2inc.org
- * Created    : April 17, 2015
- * Modified   : August 24, 2015
- * License    : GPLv2 or later
- * License URI: license.txt
- */
+    /**
+     * Version    : 1.0.1
+     * Author     : inc2734
+     * Author URI : http://2inc.org
+     * Created    : April 17, 2015
+     * Modified   : August 24, 2015
+     * License    : GPLv2 or later
+     * License URI: license.txt
+     */
 ?>
-		<?php do_action( 'habakiri_after_contents_content' ); ?>
-	<!-- end #contents --></div>
-	<footer id="footer" class="footer clearfix">
-		<nav class="footer-nav">
-			<ul>
-				<li><a href="#">会社概要</a></li>
-				<li><a href="#">お問い合わせ</a></li>
-				<li><a href="#">コラム</a></li>
-			</ul>
-		</nav>
-		<nav class="footer-sns">
-			<ul>
-				<li><a href="#"><i class="sns-icon fab fa-twitter"></i></a></li>
-				<li><a href="#"><i class="sns-icon fab fa-facebook-f"></i></a></li>
-				<li><a href="#"><i class="sns-icon fab fa-instagram"></i></a></li>
-			</ul>
-		</nav>
-		<?php do_action( 'habakiri_before_footer_content' ); ?>
+<?php do_action( 'habakiri_after_contents_content' ); ?>
+<!-- end #contents --></div>
+<footer id="footer" class="footer clearfix">
+  <nav class="footer-nav">
+	<ul>
+	  <li><a href="#">会社概要</a></li>
+	  <li><a href="#">お問い合わせ</a></li>
+	  <li><a href="#">コラム</a></li>
+	</ul>
+  </nav>
+  <nav class="footer-sns">
+	<ul>
+	  <li><a href="#" target="_blank"><i class="sns-icon fab fa-twitter"></i></a></li>
+	  <!-- <li><a href="#"><i class="sns-icon fab fa-facebook-f"></i></a></li> -->
+	  <!-- <li><a href="#"><i class="sns-icon fab fa-instagram"></i></a></li> -->
+	</ul>
+  </nav>
+  <?php do_action( 'habakiri_before_footer_content' ); ?>
 
-		<?php if ( is_active_sidebar( 'footer-widget-area' ) ) : ?>
-		<div class="footer-widget-area">
-			<div class="container">
-				<div class="row">
-					<?php dynamic_sidebar( 'footer-widget-area' ); ?>
-				<!-- end .row --></div>
-			<!-- end .container --></div>
+  <?php if ( is_active_sidebar( 'footer-widget-area' ) ) : ?>
+	<div class="footer-widget-area">
+	  <div class="container">
+		<div class="row">
+		  <?php dynamic_sidebar( 'footer-widget-area' ); ?>
+		  <!-- end .row --></div>
+		<!-- end .container --></div>
 		<!-- end .footer-widget-area --></div>
-		<?php endif; ?>
+  <?php endif; ?>
 
-		<?php if ( has_nav_menu( 'social-nav' ) ) : ?>
-		<div class="social-nav">
-			<div class="container">
-				<?php
-				wp_nav_menu( array(
-					'theme_location' => 'social-nav',
-					'fallback_cb'    => '',
-					'depth'          => 1
-				) );
-				?>
-			<!-- end .container --></div>
-		<!-- end .social-nav --></div>
-		<?php endif; ?>
+  <?php if ( has_nav_menu( 'social-nav' ) ) : ?>
+	<div class="social-nav">
+	  <div class="container">
+		<?php
+			wp_nav_menu( array(
+			  'theme_location' => 'social-nav',
+			  'fallback_cb'    => '',
+			  'depth'          => 1
+			) );
+		?>
+		<!-- end .container --></div>
+	  <!-- end .social-nav --></div>
+  <?php endif; ?>
 
-		<p class="copyright">
-		© 2018 teamclap
-		<!-- end .copyright --></p>
-		<?php do_action( 'habakiri_after_footer_content' ); ?>
-	<!-- end #footer --></footer>
+  <p class="copyright">
+	© 2018 teamclap
+	<!-- end .copyright --></p>
+  <?php do_action( 'habakiri_after_footer_content' ); ?>
+  <!-- end #footer --></footer>
 <!-- end #container --></div>
-<a id="backTop" href="#"><i class="fas fa-angle-up"></i></a>
-<?php wp_footer(); ?>
+  <a id="backTop" href="#"><i class="fas fa-angle-up"></i></a>
+  <?php wp_footer(); ?>
 </body>
 </html>
--- home.php
+++ home.php
@@ -1,132 +1,118 @@
 <?php get_header(); ?>
-     <header id="header">
-       <div class="top-header clearfix">
-         <a href="#">
-          <h1 class="logo">TEAM CLAP</h1>
-          <img class="h1-img" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.jpg" alt="teamclap">
-         </a>
-          <nav class="global-nav">
-            <ul>
-              <li><a class="first-item" href="#about">ABOUT</a></li>
-              <li><a href="#service">SERVICE</a></li>
-              <li><a href="#column">COLUMN</a></li>
-              <li><a href="#contact">CONTACT</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-      <div class="key-visual">
-        <p class="motto">More Creative. More Clap.</p>
-        <p class="motto-desc">吉祥寺を拠点にしたWeb制作・システム開発のクリエイターチーム</p>
+<div class="key-visual">
+  <div class="key-visual-inner">
+    <p class="motto">More Creative. More Clap.</p>
+    <p class="motto-desc">吉祥寺を拠点にしたWeb制作・システム開発のクリエイターチーム</p>
+  </div>
+</div>
+<div class="container">
+  <section id="about" class="about inner">
+    <h2 class="h2-topic">ABOUT</h2>
+    <p class="about-desc">
+      テストテストテストてテストテスト
+      テストテストテストてテストテスト
+      テストテストテストてテストテスト
+      テストテストテストてテストテスト
+      <br>
+      <br>
+      テストテストテストてテストテスト
+      テストテストテストてテストテスト
+      テストテストテストてテストテスト
+      テストテストテストてテストテスト
+      <br>
+      <br>
+      テストテストテストてテストテスト
+      テストテストテストてテストテスト
+      テストテストテストてテストテスト
+      テストテストテストてテストテスト
+      <br>
+      <br>
+    </p>
+  </section>
+  <section id="service" class="service inner">
+    <h2 class="h2-topic">SERVICE</h2>
+    <div class="content">
+      <div class="service-item fadeIn">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/service-image.jpg" alt="">
+        <p>テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテステストテストテストテスト
+        </p>
       </div>
-     <div class="container">
-      <section id="about" class="about inner">
-       <h2 class="h2-topic">ABOUT</h2>
-          <p class="about-desc">
-           テストテストテストてテストテスト
-           テストテストテストてテストテスト
-           テストテストテストてテストテスト
-           テストテストテストてテストテスト
-           <br>
-           <br>
-           テストテストテストてテストテスト
-           テストテストテストてテストテスト
-           テストテストテストてテストテスト
-           テストテストテストてテストテスト
-           <br>
-           <br>
-           テストテストテストてテストテスト
-           テストテストテストてテストテスト
-           テストテストテストてテストテスト
-           テストテストテストてテストテスト
-           <br>
-           <br>
-         </p>
-      </section>
-      <section id="service" class="service inner">
-        <h2 class="h2-topic">SERVICE</h2>
-        <div class="content">
-          <div class="service-item fadeIn">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/service-image.jpg" alt="">
-            <p>テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテステストテストテストテスト
-            </p>
+      <div class="service-item fadeIn">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/service-image.jpg" alt="">
+        <p>テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテステストテストテストテスト
+        </p>
+      </div>
+      <div class="service-item fadeIn2">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/service-image.jpg" alt="">
+        <p>テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテステストテストテストテスト
+        </p>
+      </div>
+      <div class="service-item fadeIn2">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/service-image.jpg" alt="">
+        <p>テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテステストテストテストテスト
+        </p>
+      </div>
+    </div>
+  </section>
+  <section id="column" class="column inner">
+    <h2 class="h2-topic">COLUMN</h2>
+    <div class="column-item-box">
+      <div class="column-item">
+        <a href="#">
+          <div class="column-img-box">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/column-img.jpg" alt="">
           </div>
-          <div class="service-item fadeIn">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/service-image.jpg" alt="">
-            <p>テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテステストテストテストテスト
-            </p>
+          <h3>テストテストテストテストテストテストテストテストテストテストテストテスト</h3>
+        </a>
+      </div>
+      <div id="popup" class="column-item">
+        <a href="#">
+          <div class="column-img-box">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/column-img.jpg" alt="">
           </div>
-          <div class="service-item fadeIn2">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/service-image.jpg" alt="">
-            <p>テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテステストテストテストテスト
-            </p>
+          <h3>テストテストテストテストテストテストテストテストテストテストテストテスト</h3>
+        </a>
+      </div>
+      <div class="column-item">
+        <a href="#">
+          <div class="column-img-box">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/column-img.jpg" alt="">
           </div>
-          <div class="service-item fadeIn2">
-            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/service-image.jpg" alt="">
-            <p>テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテステストテストテストテスト
-            </p>
+          <h3>テストテストテストテストテストテストテストテストテストテストテストテスト</h3>
+        </a>
+      </div>
+      <div id="popup" class="column-item">
+        <a href="#">
+          <div class="column-img-box">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/column-img.jpg" alt="">
           </div>
-        </div>
-      </section>
-      <section id="column" class="column inner">
-       <h2 class="h2-topic">COLUMN</h2>
-          <div class="column-item-box">
-           <div class="column-item">
-             <a href="#">
-              <div class="column-img-box">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/column-img.jpg" alt="">
-              </div>
-              <h3>テストテストテストテストテストテストテストテストテストテストテストテスト</h3>
-             </a>
-           </div>
-           <div id="popup" class="column-item">
-             <a href="#">
-               <div class="column-img-box">
-                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/column-img.jpg" alt="">
-               </div>
-               <h3>テストテストテストテストテストテストテストテストテストテストテストテスト</h3>
-             </a>
-           </div>
-           <div class="column-item">
-             <a href="#">
-               <div class="column-img-box">
-                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/column-img.jpg" alt="">
-               </div>
-               <h3>テストテストテストテストテストテストテストテストテストテストテストテスト</h3>
-             </a>
-           </div>
-           <div id="popup" class="column-item">
-             <a href="#">
-               <div class="column-img-box">
-                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/column-img.jpg" alt="">
-               </div>
-               <h3>テストテストテストテストテストテストテストテストテストテストテストテスト</h3>
-             </a>
-           </div>
-           <div class="column-item">
-             <a href="#">
-               <div class="column-img-box">
-                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/column-img.jpg" alt="">
-               </div>
-               <h3>テストテストテストテストテストテストテストテストテストテストテストテスト</h3>
-             </a>
-           </div>
-           <div id="popup" class="column-item">
-             <a href="#">
-               <div class="column-img-box">
-                 <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/column-img.jpg" alt="">
-               </div>
-               <h3>テストテストテストテストテストテストテストテストテストテストテストテスト</h3>
-             </a>
-           </div>
+          <h3>テストテストテストテストテストテストテストテストテストテストテストテスト</h3>
+        </a>
+      </div>
+      <div class="column-item">
+        <a href="#">
+          <div class="column-img-box">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/column-img.jpg" alt="">
           </div>
-      </section>
-     </div><!--container-->
-      <section id="contact" class="contact">
-       <h2 class="h2-topic">CONTACT</h2>
-       <p>HP作成やシステム開発などのお仕事のご依頼は、お気軽にご相談ください。</p>
-       <a class="ToContactForm" href="#">お問い合わせ</a>
-      </section>
-    <?php get_footer(); ?>
+          <h3>テストテストテストテストテストテストテストテストテストテストテストテスト</h3>
+        </a>
+      </div>
+      <div id="popup" class="column-item">
+        <a href="#">
+          <div class="column-img-box">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/column-img.jpg" alt="">
+          </div>
+          <h3>テストテストテストテストテストテストテストテストテストテストテストテスト</h3>
+        </a>
+      </div>
+    </div>
+  </section>
+</div><!--container-->
+<section id="contact" class="contact">
+  <h2 class="h2-topic">CONTACT</h2>
+  <p>HP作成やシステム開発などのお仕事のご依頼は、お気軽にご相談ください。</p>
+  <a class="ToContactForm" href="#">お問い合わせ</a>
+</section>
+<?php get_footer(); ?>
   </body>
 </html>
